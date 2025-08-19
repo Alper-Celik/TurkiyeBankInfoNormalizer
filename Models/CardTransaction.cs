01684b26c8249ac5a7b609847cc19b61f7c3844b
Alper Çelik <alper@alper-celik.dev>
2025-08-19 20:04:20 +0300

    improve csv output

--- Models/CardTransaction.cs
+++ Models/CardTransaction.cs
@@ -1,14 +1,29 @@
+using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
 namespace Models;
 
 public class CardTransaction
 {
-    public int Id { get; set; }
+    [Format("o")]
     public required DateOnly TransactionDate { get; set; }
+
+    [Format("o")]
     public TimeOnly? TransactionTime { get; set; }
+
     public long AmountInMinorUnit { get; set; }
+
     public required string Comment { get; set; }
+
+    [HeaderPrefix("Currency.")]
     public required Currency.Currency Currency { get; set; }
+
+    [HeaderPrefix("Country.")]
     public Country.Country? Country { get; set; }
+
+    [HeaderPrefix("Card.")]
     public required Card Card { get; set; }
+
     public string? Category { get; set; }
 }
